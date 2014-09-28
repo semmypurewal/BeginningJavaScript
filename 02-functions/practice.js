a46d39a005bcdc57327c2c132fd9b44b9933d59d
Semmy Purewal <semmypurewal@gmail.com>
2014-09-28 15:56:01 -0700

    add solutions for section 2

--- 02-functions/practice.js
+++ 02-functions/practice.js
@@ -1,19 +1,31 @@
-var add = function () {
+var add = function (a, b) {
+    var sum = a + b;
+    return sum;
 };
 
-var totalCost = function () {
+var totalCost = function (quantity, price) {
+    var total = quantity * price;
+    return total;
 };
 
-var cardString = function () {
+var cardString = function (rank, suit) {
+    var cardString = rank + " of " + suit;
+    return cardString;
 };
 
-var openTag = function () {
+var openTag = function (tag) {
+    var open = "<" + tag + ">";
+    return open;
 };
 
-var closeTag = function () {
+var closeTag = function (tag) {
+    var close = "</" + tag + ">";
+    return close;
 };
 
-var toTagString = function () {
+var toTagString = function (tag, content) {
+    var result = openTag(tag) + content + closeTag(tag);
+    return result;
 };
 
 
