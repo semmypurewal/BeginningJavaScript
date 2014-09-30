c8a2aae013cd323ab994b13fcc7639d7363fbd49
Semmy Purewal <semmypurewal@gmail.com>
2014-09-29 18:05:44 -0700

    add stubs for practice.js in poker

---
+++ 09-poker-simulation/practice.js
@@ -0,0 +1,64 @@
+var suits = ["clubs", "diamonds", "hearts", "spades"];
+var ranks = ["two", "three", "four", "five", "six", "seven", "eight",
+             "nine", "ten", "jack", "queen", "king", "ace"];
+
+var isSuit = function () {
+};
+
+var isRank = function () {
+};
+
+var isCard = function () {
+};
+
+var isDeck = function () {
+};
+
+var createDeck = function () {
+};
+
+var shuffle = function () {
+};
+
+var isHand = function () {
+};
+
+var dealHand = function () {
+};
+
+var isHigherThan = function () {
+};
+
+var isLowerThan = function () {
+};
+
+var highCard = function () {
+};
+
+var lowCard = function () {
+};
+
+var containsPair = function () {
+};
+
+var containsTwoPair = function () {
+};
+
+var containsThreeOfAKind = function () {
+};
+
+var containsFullHouse = function () {
+};
+
+var containsStraight = function () {
+};
+
+var containsFlush = function () {
+};
+
+var containsStraightFlush = function () {
+};
+
+var containsRoyalFlush = function () {
+};
+
