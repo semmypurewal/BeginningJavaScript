964cf8e16aaec9cc440e99994ac5bfde910d1d07
Semmy Purewal <semmypurewal@gmail.com>
2014-10-01 21:08:09 -0700

    add tests for random functions

--- 03-types/tests.js
+++ 03-types/tests.js
@@ -8,15 +8,58 @@ describe("#isDivisibleBy3", function () {
 });
 
 describe("#celsToFahr", function () {
+    it ("should convert celsius to fahrenheit", function () {
+        expect(celsToFahr(100)).toBe(212);
+        expect(celsToFahr(30)).toBe(86);
+        expect(celsToFahr(0)).toBe(32);
+    });
 });
 
 describe("#fahrToCels", function () {
+    it ("should convert fahrenheit to celsius", function () {
+        expect(fahrToCels(212)).toBe(100);
+        expect(fahrToCels(86)).toBe(30);
+        expect(fahrToCels(32)).toBe(0);
+    });
 });
 
 describe("#randUpTo", function () {
+    it ("should never include the number that is input", function () {
+        var i;
+        for (i = 0; i < 100; i++) {
+            expect(randUpTo(10)).toBeLessThan(10);
+        }
+    });
+
+    it ("should always be smaller than the number input", function () {
+        var i;
+        for (i = 0; i < 100; i++) {
+            expect(randUpTo(100)).toBeLessThan(100);
+        }
+    });
+
+    it ("should always be bigger than -1", function () {
+        var i;
+        for (i = 0; i < 100; i++) {
+            expect(randUpTo(1000)).toBeGreaterThan(-1);
+        }
+    });
 });
 
 describe("#randBetween", function () {
+    it ("should always be larger than or equal to the low number", function () {
+        var i;
+        for (i = 0; i < 1000; i++) {
+            expect(randBetween(10, 50)).toBeGreaterThan(9);
+        }
+    });
+
+    it ("should always be less than the high number", function () {
+        var i;
+        for (i = 0; i < 1000; i++) {
+            expect(randBetween(10, 50)).toBeLessThan(50);
+        }
+    });
 });
 
 describe("#isSuit", function () {
