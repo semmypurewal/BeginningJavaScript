531f574e737fa5d7a5ff57c15d31d7b233e0704c
Semmy Purewal <semmypurewal@gmail.com>
2014-10-01 20:55:47 -0700

    add first test to section 3

--- 03-types/tests.js
+++ 03-types/tests.js
@@ -1,11 +1,35 @@
-describe ("#celsToFahr", function () {
-    it ("should correctly convert celsius to fahrenheit", function () {
-        expect(false).toBe(true);
+describe("#isDivisibleBy3", function () {
+    it ("should return true if the number is divisible by 3, false otherwise", function () {
+        expect(isDivisibleBy3(3)).toBe(true);
+        expect(isDivisibleBy3(5)).toBe(false);
+        expect(isDivisibleBy3(36)).toBe(true);
+        expect(isDivisibleBy3(0)).toBe(true);
     });
 });
 
-describe ("#fahrToCels", function () {
-    it ("should correctly convert fahrenheit to celsius", function () {
-        expect(false).toBe(true);
-    });
+describe("#celsToFahr", function () {
+});
+
+describe("#fahrToCels", function () {
+});
+
+describe("#randUpTo", function () {
+});
+
+describe("#randBetween", function () {
+});
+
+describe("#isSuit", function () {
+});
+
+describe("#isRank", function () {
+});
+
+describe("#isCard", function () {
+});
+
+describe("#getHTMLText", function () {
+});
+
+describe("#isHTMLElement", function () {
 });
