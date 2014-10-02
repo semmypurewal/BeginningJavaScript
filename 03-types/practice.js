ea0a3aa95d17c0ac12b2f24b9f2d6f92bb535578
Semmy Purewal <semmypurewal@gmail.com>
2014-10-01 21:15:29 -0700

    add solutions to part of section 3

--- 03-types/practice.js
+++ 03-types/practice.js
@@ -1,5 +1,54 @@
-var celsToFahr = function () {
+var isDivisibleBy3 = function (val) {
+    return val % 3 === 0;
 };
 
-var fahrToCels = function () {
+var celsToFahr = function (f) {
+    return (9/5) * f + 32;
 };
+
+var fahrToCels = function (c) {
+    return (c - 32) * (5/9);
+};
+
+var randUpTo = function (n) {
+    return Math.floor(Math.random() * n);
+};
+
+var randBetween = function (a, b) {
+    return Math.floor(Math.random() * (b - a) + a);
+};
+
+var isSuit = function (s) {
+    var lowerCaseSuit = s.toLowerCase();
+    return lowerCaseSuit === "hearts" ||
+        lowerCaseSuit === "spades" ||
+        lowerCaseSuit === "clubs" ||
+        lowerCaseSuit === "diamonds";
+};
+
+var isRank = function (r) {
+    var lowerCaseRank = r.toLowerCase();
+    return lowerCaseRank === "two" ||
+        lowerCaseRank === "three" ||
+        lowerCaseRank === "four" ||
+        lowerCaseRank === "five" ||
+        lowerCaseRank === "six" ||
+        lowerCaseRank === "seven" ||
+        lowerCaseRank === "eight" ||
+        lowerCaseRank === "nine" ||
+        lowerCaseRank === "ten" ||
+        lowerCaseRank === "jack" ||
+        lowerCaseRank === "queen" ||
+        lowerCaseRank === "king" ||
+        lowerCaseRank === "ace";
+};
+
+var isCard = function () {
+};
+
+var getHTMLText = function () {
+};
+
+var isHTMLElement = function () {
+};
+>>>>>>> Stashed changes
