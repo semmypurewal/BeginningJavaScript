c13cfab9c5b17b753c6c68b36eb717f780ecfc64
Semmy Purewal <semmypurewal@gmail.com>
2014-10-01 20:49:30 -0700

    stub out practice problems

--- 03-types/practice.js
+++ 03-types/practice.js
@@ -1,5 +1,29 @@
+var isDivisibleBy3 = function () {
+};
+
 var celsToFahr = function () {
 };
 
 var fahrToCels = function () {
+};
+
+var randUpTo = function () {
+};
+
+var randBetween = function () {
+};
+
+var isSuit = function () {
+};
+
+var isRank = function () {
+};
+
+var isCard = function () {
+};
+
+var getHTMLText = function () {
+};
+
+var isHTMLElement = function () {
 };
