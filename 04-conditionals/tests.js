dbfc89a6108675019f8f4357d0dbfd9883cfdbc6
Semmy Purewal <semmypurewal@gmail.com>
2014-10-04 12:43:26 -0700

    add initial tests.js file

---
+++ 04-conditionals/tests.js
@@ -0,0 +1,26 @@
+describe("#isLeapYear", function () {
+});
+
+describe("#passwordStrength", function () {
+});
+
+describe("#firstInDictionary", function () {
+});
+
+describe("#getTagName", function () {
+});
+
+describe("#isQuestion", function () {
+});
+
+describe("#magic8Ball", function () {
+});
+
+describe("#improveTweet", function () {
+});
+
+describe("#interjectAt", function () {
+});
+
+describe("#randomInterject", function () {
+});
