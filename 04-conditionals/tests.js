54019df058346661cbb891273a6e9a900f8bb55a
Semmy Purewal <semmypurewal@gmail.com>
2014-10-04 12:57:24 -0700

    update tests in section 4

--- 04-conditionals/tests.js
+++ 04-conditionals/tests.js
@@ -1,4 +1,20 @@
 describe("#isLeapYear", function () {
+    it("should return true if the year is a leap year, false otherwise", function () {
+        expect(isLeapYear(1988)).toBe(true);
+        expect(isLeapYear(2001)).toBe(false);
+        expect(isLeapYear(1800)).toBe(false);
+        expect(isLeapYear(2000)).toBe(true);
+    });
+
+    it("should throw an input is not a number", function () {
+        expect(function () {
+            isLeapYear("string!");
+        }).toThrow();
+
+        expect(function () {
+            isLeapYear(true);
+        }).toThrow();
+    });
 });
 
 describe("#passwordStrength", function () {
