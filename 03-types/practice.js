47c319e98dbd0510171d39417e05e2c3aa666047
Semmy Purewal <semmypurewal@gmail.com>
2014-10-05 12:03:06 -0700

    move prob descriptions into practice.js for section 3

--- 03-types/practice.js
+++ 03-types/practice.js
@@ -1,29 +1,117 @@
+// Write a function called isDivisibleBy3 which returns `true` if a number is
+// divisible by 3, and `false` otherwise.
 var isDivisibleBy3 = function () {
 };
 
+
+// Write a function that converts a Celsius temperature to Fahrenheit,
+// and vice-versa. To convert from Celsius to Fahrenheit, you multiply
+// the celsius value by 9 and then divide by 5. Then you add 32. To
+// convert the other way, you subtract 32, and then multiply by
+// 9. Finally, you divide by 5. The division operator in JavaScript is
+// `/`.
 var celsToFahr = function () {
 };
 
 var fahrToCels = function () {
 };
 
+
+// Write a function called `randUpTo` that accepts a number and returns a random
+// whole number between 0 and that number. For example:
+//
+//     randUpTo(10);
+//     //=> 5
+//
+//     randUpTo(10);
+//     //=> 9
+//
+//     randUpTo(1000);
+//     //=> 561
+//
+//     randUpTo(1000);
+//     //=> 236
 var randUpTo = function () {
 };
 
+
+// Write a function called `randBetween` that accepts two numbers representing a
+// range and returns a random whole number between those two numbers.
 var randBetween = function () {
 };
 
+
+// The standard card suits are clubs, diamonds, hearts and spades. Write a
+// function called isSuit that accepts a string and returns true if the input
+// string is a suit, and false otherwise. Consider making a more robust function
+// that will allow the case to be arbitrary, meaning "HEARTS", "hearts", and
+// "hEaRtS" all return `true`. You can use the `toLowerCase` or `toUpperCase`
+// string methods to achieve this.
+//
+//     isSuit("hearts");
+//     //=> true
+//
+//     isSuit("HEARTS");
+//     //=> true
+//
+//     isSuit("coins");
+//     //=> false
 var isSuit = function () {
 };
 
+
+// The valid ranks for a card are two, three, four, five, six, seven, eight,
+// nine, ten, jack, queen, king and ace. Write a function called `isRank` that
+// accepts a string and returns true if it is a card rank.
+//
+//     isRank("jack");
+//     //=> true
+//
+//     isRank("Queen");
+//     //=> true
+//
+//     isRank("one");
+//     //=> false
 var isRank = function () {
 };
 
+
+// Using the previous two functions, write a function called isCard that accepts
+// two arguments, a suit and a rank, and returns true if they are valid for a card,
+// and false otherwise.
 var isCard = function () {
 };
 
+
+// Write a function that accepts a string representation of an HTML element (it
+// can't have nested HTML elements) and returns the string contained inside. For
+// example:
+//
+//     getHTMLText("<p>this is some text in a paragraph</p>");
+//     //=> this is some text
+//
+//     getHTMLText("<li>this is a list item</li>");
+//     //=> this is a list item
 var getHTMLText = function () {
 };
 
+
+// Write a function that determines if a string represents an HTML element. This
+// means it has to start with an opening HTML tag and end with a closing HTML tag.
+//
+//     isHTMLElement("<p>this is a paragraph</p>");
+//     //=> true
+//
+//     isHTMLElement("this is a tweet!");
+//     //=> false
+//
+//     isHTMLElement("<p>this is NOT an paragraph</div>");
+//     //=> false
+//
+//     isHTMLElement("<li>this is an HTML list element</li>");
+//     //=> true
+//
+// It may help in this case to look up the `lastIndexOf` method on the string
+// objects.
 var isHTMLElement = function () {
 };
