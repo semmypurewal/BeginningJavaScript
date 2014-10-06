50825a50c3a242fe7e3af86a73b33926ac34b18a
Semmy Purewal <semmypurewal@gmail.com>
2014-10-05 18:33:08 -0700

    add first drafts of tests and runner to section 4

---
+++ 05-loops/tests.js
@@ -0,0 +1,30 @@
+describe("#isVowel", function () {
+});
+
+describe("#isLowerCaseLetter", function () {
+});
+
+describe("#sumUpTo", function () {
+});
+
+describe("#sumAToB", function () {
+});
+
+describe("#countVowels", function () {
+});
+
+describe("#isPrime", function () {
+});
+
+describe("#sumPrimesUpTo", function () {
+});
+
+describe("#sumOfFirstNPrimes", function () {
+});
+
+describe("#removeNonLetters", function () {
+});
+
+describe("#isPalindrome", function () {
+});
+
