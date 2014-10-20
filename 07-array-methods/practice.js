b53bc35824f9eff3567e556fa6a8dec7ea9074e7
Semmy Purewal <semmypurewal@gmail.com>
2014-10-19 18:17:15 -0700

    add first draft of practice problems to section 7

---
+++ 07-array-methods/practice.js
@@ -0,0 +1,79 @@
+// In one of the previous sections, we had an practice problem where you had to
+// reverse a string. Do the same thing with an array, but use the `reduce` and
+// `concat` methods to avoid local variables.
+//
+//      reverse([ 1, 2, 3, 4, 5 ]);
+//      //=> [ 5, 4, 3, 2, 1 ]
+//
+//      reverse([ "hello", "world" ]);
+//      //=> [ "world", "hello" ]
+var reverse = function () {
+};
+
+// Did you know that you could have arrays within arrays? This is perfectly
+// legal JavaScript:
+//
+//     var nestedArray = [ 1, 2, [ 10, 20 ], 3, 4, 5 ];
+//
+//     nestedArray[0];
+//     //=> 1
+//
+//     nestedArray[1];
+//     //=> 2
+//
+//     nestedArray[2];
+//     //=> [ 10, 20 ]
+//
+//     nestedArray[2][0];
+//     //=> 10
+//
+// For this problem, write a function using `reduce` that "flattens" a possibly
+// nested array into a single array.
+//
+//     flatten(nestedArray);
+//     //=> [ 1, 2, 10, 20,  3, 4, 5 ]
+//
+//     flatten([ 1, [2, 3], 4, [5, 6, 7], 8 ]);
+//     //=> [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+//
+// You'll also want to use the `concat` method to make this work.
+var flatten = function () {
+};
+
+// Using `range` and a chain of array methods, write a function that accepts a
+// number `n` and returns the sum of all of the positive multiples of 3 and 5 that
+// are smaller than or equal to `n`.
+var sumOfMultiplesOf3And5 = function () {
+};
+
+// Write a function called atLeastOneVowel that accepts a string and
+// returns true if that word contains at least one vowel. Do not use a
+// `for` loop or a `forEach` loop.
+var atLeastOneVowel = function () {
+};
+
+// Write a function that accepts a list of tweets, and returns the longest tweet
+// that contains the word `awesome`.
+var longestAwesome = function () {
+};
+
+// Write a function that accepts an array of HTMLElements and returns an
+// array of their content.
+var elementsToContent = function () {
+};
+
+// In a previous section, we created a function called `randUpTo` that
+// returned a random integer up to an upper bound. Using that function
+// along with the `range` function, write a method called
+// `randomArray` that accepts two numbers, `length`, and `max`. It
+// should return an array of length `length` that contains random
+// numbers up to the value `max`.
+var randomArray = function () {
+};
+
+// Using the `randomNums` function from above, write a function called
+// `randomElements` that accepts an array, and a number, `n` and
+// returns a new array that consists of `n` random elements selected
+// from the input array.
+var randomElements = function () {
+};
