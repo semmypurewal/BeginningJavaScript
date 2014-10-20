a1d5c22bab5f6512cc43f4d9657233b1394b679c
Semmy Purewal <semmypurewal@gmail.com>
2014-10-19 18:51:53 -0700

    add test stubs for section 7

---
+++ 07-array-methods/tests.js
@@ -0,0 +1,34 @@
+describe("#reverse", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#flatten", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#sumOfMultiplesOf3And5", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#atLeastOneVowel", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#longestAwesomeTweet", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#randomArray", function () {
+    it ("should do stuff", function () {
+    });
+});
+
+describe("#randomElements", function () {
+    it ("should do stuff", function () {
+    });
+});
