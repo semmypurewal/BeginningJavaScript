703e90e9d7c18015e9361b17396398590bdee703
Semmy Purewal <semmypurewal@gmail.com>
2014-10-19 18:41:48 -0700

    flesh out the practice problems in section 7

--- 07-array-methods/practice.js
+++ 07-array-methods/practice.js
@@ -7,6 +7,7 @@
 //
 //      reverse([ "hello", "world" ]);
 //      //=> [ "world", "hello" ]
+//
 var reverse = function () {
 };
 
@@ -37,28 +38,64 @@ var reverse = function () {
 //     //=> [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 //
 // You'll also want to use the `concat` method to make this work.
+//
 var flatten = function () {
 };
 
 // Using `range` and a chain of array methods, write a function that accepts a
 // number `n` and returns the sum of all of the positive multiples of 3 and 5 that
 // are smaller than or equal to `n`.
+//
+//     sumOfMultiplesOf3And5(100);
+//     //=> 2318
+//
+//     sumOfMultiplesOf3And5(50);
+//     //=> 543
+//
+//     sumOfMultiplesOf3And5(0);
+//     //=> 0
+//
 var sumOfMultiplesOf3And5 = function () {
 };
 
 // Write a function called atLeastOneVowel that accepts a string and
 // returns true if that word contains at least one vowel. Do not use a
 // `for` loop or a `forEach` loop.
+//
+//     atLeastOneVowel("hello");
+//     //=> true
+//
+//     atLeastOneVowel("dry");
+//     //=> false
+//
+//     atLeastOneVowel("sdfjkl");
+//     //=> false
+//
 var atLeastOneVowel = function () {
 };
 
-// Write a function that accepts a list of tweets, and returns the longest tweet
-// that contains the word `awesome`.
-var longestAwesome = function () {
+// Write a function that accepts a list of tweets, and returns the
+// longest tweet that contains the word `awesome` or the empty string
+// if no tweet contains the word awesome.
+//
+//     logestAwesomeTweet([ "awesome", "longer tweet with awesome", "not awesome", "empty" ]);
+//     //=> "longer tweet with awesome"
+//
+//     longestAwesomeTweet([ "hello", "world" ]);
+//     //=> ""
+//
+var longestAwesomeTweet = function () {
 };
 
 // Write a function that accepts an array of HTMLElements and returns an
 // array of their content.
+//
+//     elementsToContent(["<p>this is a paragraph</p>", "<li>list item</li>", "<a>link!</a>" ]);
+//     //=> [ "this is a paragraph", "list item", "link!" ]
+//
+//     elementsToContent([ "<h1>This is an important heading!</h1>", "<h5>this is not as important</h5>" ]);
+//     //=> [ "This is an important heading!", "this is not as important" ]
+//
 var elementsToContent = function () {
 };
 
@@ -68,12 +105,26 @@ var elementsToContent = function () {
 // `randomArray` that accepts two numbers, `length`, and `max`. It
 // should return an array of length `length` that contains random
 // numbers up to the value `max`.
+//
+//     randomArray(10, 100);
+//     //=> [ 56, 32, 4, 92, 73, 75, 11, 10, 26, 4 ]
+//
+//     randomArray(5, 10);
+//     //=> [ 2, 0, 3, 9, 10 ]
+//
 var randomArray = function () {
 };
 
 // Using the `randomNums` function from above, write a function called
 // `randomElements` that accepts an array, and a number, `n` and
 // returns a new array that consists of `n` random elements selected
-// from the input array.
+// from the input array. Duplicates are allowed.
+//
+// randomElements([ "red", "orange", "yellow", "green", "blue", "purple", "gray", "black", "white" ], 5);
+// //=> [ "green", "green", "blue", "orange", "black" ]
+//
+// randomElements([ "clubs", "diamonds", "hearts", "spades" ], 3);
+// //=> [ "hearts", "diamonds", "hearts" ]
+//
 var randomElements = function () {
 };
