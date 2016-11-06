93a7acff7c6399740b2def9be1ed7af0e819fae0
Jared Wallace <jared@jared-wallace.com>
2016-11-05 19:27:13 -0500

    Clarified the instructions for totalCost

--- 02-functions/practice.js
+++ 02-functions/practice.js
@@ -3,7 +3,7 @@ var add = function () {
 };
 
 
-// Create a function that takes in a cost and a quantity and outputs the total
+// Create a function that takes in a cost and a quantity and returns the total
 // pre-tax cost for that quantity of items at the given price. For example
 //
 //     var preTaxTotal = totalCost(5, 5.99); // 5 items at 5.99
